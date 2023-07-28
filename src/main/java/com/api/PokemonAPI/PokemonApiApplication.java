53827b6e6d4be7c3bd763d106841caf71754ed7f
Celso Machado <celsobarrosmachado@gmail.com>
2023-07-28 20:51:18 +0100

    First Version

---
+++ src/main/java/com/api/PokemonAPI/PokemonApiApplication.java
@@ -0,0 +1,16 @@
+package com.api.PokemonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonApiApplication.class, args);
+
+		System.out.println("TESTEEE");
+
+	}
+
+}
---
+++ src/test/java/com/api/PokemonAPI/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.PokemonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
